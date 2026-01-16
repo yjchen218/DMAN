a08d0bb02d1ba9a2cd344b40a0e045ac2a6a0061
yjchen218 <yjchen@mail.ustc.edu.cn>
2026-01-16 17:54:33 +0800

    Update DMAN_Train.py

--- DMAN_Train.py
+++ DMAN_Train.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# file: train.py
-# author: songyouwei <youwei0314@gmail.com> (Final Verified Version)
-# Copyright (C) 2018. All Rights Reserved.
-
 import logging
 import argparse
 import math
